690bc42393f7b637d8bb6facc623b4015bebf2fc
Damien Corbell <damien@northshoreautomation.com>
2026-02-13 17:13:43 -0800

    Initial Commit

---
+++ LaunchControl/LaunchControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchControlApp.swift
+//  LaunchControl
+//
+//  Created by Damien Corbell on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct LaunchControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaunchControlTests/LaunchControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchControlTests.swift
+//  LaunchControlTests
+//
+//  Created by Damien Corbell on 2/13/26.
+//
+
+import Testing
+@testable import LaunchControl
+
+struct LaunchControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
